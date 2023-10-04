938a65b058ffa4b0a0f12d33533411b00e2e4bfd
Muhammad Asim <asimkamal251@gmail.com>
2023-10-04 23:44:06 +0500

    fix : code change from writing txt file to post data on a fake api

--- MultithreadingExample/Program.cs
+++ MultithreadingExample/Program.cs
@@ -3,20 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MultithreadingExample
 {
     class Program
     {
+        private static readonly string getApiUrl = "https://countrycode.org/api/countryCode/countryMenu";
+        private static readonly string postApiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+        static async Task<List<DataModel>> FetchDataFromApi(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string json = await client.GetStringAsync(apiUrl);
+                List<DataModel> dataList = JsonConvert.DeserializeObject<List<DataModel>>(json);
+                return dataList;
+            }
+        }
+
         static async Task Main(string[] args)
         {
-            string apiUrl = "https://countrycode.org/api/countryCode/countryMenu";
-
             try
             {
-                List<DataModel> dataList = await FetchDataFromApi(apiUrl);
-                await WriteDataToFileAsync(dataList);
+                List<DataModel> dataList = await FetchDataFromApi(getApiUrl);
+                await PostDateToApi(dataList);
 
                 Console.WriteLine("Processing complete.");
 
@@ -29,35 +41,37 @@ namespace MultithreadingExample
             Console.Read();
         }
 
-        static async Task<List<DataModel>> FetchDataFromApi(string apiUrl)
+
+        static async Task PostDateToApi(List<DataModel> dataList)
         {
-            using (HttpClient client = new HttpClient())
+            foreach (var dataItem in dataList)
             {
-                string json = await client.GetStringAsync(apiUrl);
-                List<DataModel> dataList = JsonConvert.DeserializeObject<List<DataModel>>(json);
-                return dataList;
-            }
-        }
-
-        static async Task WriteDataToFileAsync(List<DataModel> dataList)
-        {
-            string execPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
-#if DEBUG
-            // Remove the debug bin folder from the project directory.
-            execPath = execPath.Replace("file:\\", "");
-            execPath = execPath.Replace("\\bin\\Debug", "");
-#endif
-
-            string filePath = Path.Combine(execPath, "output.txt");
-
-            using (StreamWriter writer = new StreamWriter(filePath, true))
-            {
-                await writer.WriteLineAsync("-------------------------br-------------------------");
-
-                foreach (var dataItem in dataList)
+                var postData = new
                 {
-                    string serializedData = JsonConvert.SerializeObject(dataItem);
-                    await writer.WriteLineAsync(serializedData);
+                    title = dataItem.name,
+                    body = dataItem.path,
+                    userId = dataItem.code
+                };
+
+                string json = JsonConvert.SerializeObject(postData);
+
+                using (HttpClient client = new HttpClient())
+                {
+                    // Create a new StringContent object and set the "Content-Type" header here
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    // Send the POST request with the content
+                    HttpResponseMessage response = await client.PostAsync(postApiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseJson = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseJson);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
                 }
             }
         }
