653021f48639315033c00c4db460621c24786c95
Muhammad Asim <asimkamal251@gmail.com>
2023-10-15 20:15:14 +0500

    fix : products controller added

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET api/products
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "products1", "products2" };
+        }
+
+        // GET api/products/5
+        public string Get(int id)
+        {
+            return "products";
+        }
+
+        // POST api/products
+        public void Post([FromBody] string products)
+        {
+        }
+
+        // PUT api/products/5
+        public void Put(int id, [FromBody] string products)
+        {
+        }
+
+        // DELETE api/products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
